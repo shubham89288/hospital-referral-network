e3f53bf81b72be4f9a819ded379e0686336250f6
shubham89288 <shubhamsinghyadav550@gmail.com>
2026-03-21 20:15:10 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Login</title>
+    
+<link rel="stylesheet" href="style.css">
+</head>
+   <body class="login-page">
+<div class="container">
+    <div class="form-box">
+       <h1 class="st"><u>STUDENT LOGIN</u></h1>
+
+        <form action="" method="POST">
+       
+    
+           Enter your Name:
+            <input type="email" name="email" placeholder="Enter Email" required><br><br>
+            Enter your Password:
+            <input type="password" name="password" placeholder="Enter Password" required><br><br>
+            
+            <button type="submit">Login</button>
+
+        </form>
+
+        <p class="p1">Don't have an account?  
+       <h4><a href="sign.php">Create  New Account</a></h4>
+</p>
+        
+    </div>
+</div>
+
+
+</body>
+</html>
